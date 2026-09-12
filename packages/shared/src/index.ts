0698d21ffae696e7493b6cfd60c92a4760a9b1f6
OpenArc <openarc-github.carry493@slmails.com>
2026-09-12 11:01:04 -0400

    feat(control): add protected policy HTTP and session contracts

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -31,3 +31,4 @@ export * from "./commerce/market-capabilities.js";
 export * from "./commerce/control-policy.js";
 export * from "./commerce/control-policy-wire.js";
 export * from "./commerce/control-capabilities.js";
+export * from "./commerce/control-session.js";
